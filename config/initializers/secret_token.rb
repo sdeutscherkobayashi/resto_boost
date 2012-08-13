e89ec74034b3a0aed08b2688bcc9ca0c34e58b7c
Steven Deutscher-Kobayashi <sdeutscherkobayashi@gmail.com>
2012-08-13 16:33:02 -0400

    create initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestoBoost::Application.config.secret_token = '5613309dd6d3c2c8346da1a6d38c8c32329fd2a91edb50b898c9e0ee477d125dd43aa48ccd39dbcfa6ea061ba915d8cfe575edc4e5e66fb93dd91e6c820414b6'
